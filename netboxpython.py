0e31ec267e210431473b16f9dc2713cb083626dc
HRSallepalli95 <67352332+HRSallepalli95@users.noreply.github.com>
2021-02-10 10:00:08 +1100

    Create netboxpython.py

---
+++ netboxpython.py
@@ -0,0 +1,24 @@
+import requests
+url = 'https://192.168.1.224:4430/api/dcim/devices'
+headers = { 'Authorization': "Token f857d283c8a38333198893d4d99bce13cb45a1ce"}
+r = requests.get(url, headers=headers, verify=False)
+jsondata = r.json()
+count = jsondata['count']
+i=0
+f = open("devices.yml", "w")
+type1 = 2
+type2 = 2
+while( i < count):
+        device = jsondata['results'][i]
+        deviceip = device['primary_ip']['address']
+        devicename = device['name']
+        devicegroup = device['tags'][0]
+        ip = deviceip.split('/')
+        deviceip = ip[0]
+        i= i+1
+        f.write("Switch"+str(i)+":"+'\n')
+        f.write('  '+"ip:"+' '+deviceip+'\n')
+        f.write('  '+"group:"+' '+devicegroup+'\n')
+        f.write('  '+"name:"+' '+devicename+'\n')
+        f.write('\n')
+
